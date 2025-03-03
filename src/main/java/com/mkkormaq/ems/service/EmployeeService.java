d544c81ced120def786d22a3602c8d04245908b6
Josh McCormack <joshgmc@gmail.com>
2025-03-02 20:52:39 -0500

    added get all employee service and controller endpoint

--- src/main/java/com/mkkormaq/ems/service/EmployeeService.java
+++ src/main/java/com/mkkormaq/ems/service/EmployeeService.java
@@ -1,9 +1,14 @@
 package com.mkkormaq.ems.service;
 
+import com.mkkormaq.ems.EmsBackendApplication;
 import com.mkkormaq.ems.dto.EmployeeDto;
+
+import java.util.List;
 
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
 }
--- src/main/java/com/mkkormaq/ems/service/EmployeeServiceImpl.java
+++ src/main/java/com/mkkormaq/ems/service/EmployeeServiceImpl.java
@@ -8,6 +8,9 @@ import com.mkkormaq.ems.repository.EmployeeRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService{
@@ -30,4 +33,13 @@ public class EmployeeServiceImpl implements EmployeeService{
                 .orElseThrow(()-> new ResourceNotFoundException("Employee does not exist with given ID: " + employeeId));
         return EmployeeMapper.mapToEmployeeDto(employee);
     }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee)-> EmployeeMapper.mapToEmployeeDto(employee))
+                .collect(Collectors.toList());
+    }
+
+
 }
