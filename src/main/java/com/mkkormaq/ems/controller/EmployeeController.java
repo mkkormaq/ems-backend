d1d28fe39bb99e212d5eaf847e04f23fe8e99cbe
Josh McCormack <joshgmc@gmail.com>
2025-03-03 21:31:46 -0500

    commented existing mappings in EmployeeController

--- src/main/java/com/mkkormaq/ems/controller/EmployeeController.java
+++ src/main/java/com/mkkormaq/ems/controller/EmployeeController.java
@@ -15,21 +15,27 @@ import java.util.List;
 public class EmployeeController {
     private EmployeeService employeeService;
 
+    //Creates a new employee to the database
     @PostMapping
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
         EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
+    //Gets an individual employee by ID
     @GetMapping("{id}")
     public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId){
         EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
         return ResponseEntity.ok(employeeDto);
     }
 
+    //Gets all employees and returns them as a list
     @GetMapping
     public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
         List<EmployeeDto> employees = employeeService.getAllEmployees();
         return ResponseEntity.ok(employees);
     }
+
+    //Updates an existing employee in the database
+
 }
